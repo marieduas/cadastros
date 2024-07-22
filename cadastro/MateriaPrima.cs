b45aafdabffafddf6fda95f94b0d01a309f82cf0
cadastros <malvesdasi8@gmail.com>
2024-07-22 11:51:57 -0300

    começo

---
+++ cadastro/MateriaPrima.cs
@@ -0,0 +1,52 @@
+namespace cadastro;
+
+public class MateriaPrima
+{
+    int Unidade;
+    int id;
+
+    string Valor Estimado;
+    int id;
+
+    string Tempo Estimado;
+    int id;
+
+    string Preço Total;
+    int id;
+
+public void SetUnidade (int A)
+{
+    this.Unidade = A;
+}
+public int GetA ()
+{
+    return Unidade;
+}
+
+public void SetValorEstimado (string B)
+{
+    this.ValorEstimado = B;
+}
+public string GetB ()
+{
+    return ValorEstimado;
+}
+
+public void SetTempoEstimado (string C)
+{
+    this.TempoEstimado = C;
+}
+public string GetC ()
+{
+    return TempoEstimado;
+}
+
+public void SetPreçoTotal (string D)
+{
+    this.PreçoTotal = D
+}
+public string GetD ()
+{
+    return PreçoTotal
+}
+}
